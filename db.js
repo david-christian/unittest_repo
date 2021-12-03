ebe6bd3883a2369aa62e7fc762b5886b3917d1bd
david <davidyu@future.net.co>
2021-12-03 18:05:54 +0800

    init

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var db = mysql.createConnection({
-  host:process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
+  host:"192.168.34.23",
+  user: "root",
+  password: "123",
+  database: "test"
 });
 
 module.exports = db;
---
+++ test/aaa.test.js
@@ -0,0 +1,8 @@
+const aaa = require('../aaa');
+const expect = require('chai').expect;
+
+describe('加法函数的测试', function() {
+    it('1 加 1 应该等于 2', function() {
+        expect(aaa(1, 1)).to.be.equal(2);
+    });
+});
